4d4c880887e1a178fc4a56e2beab4d12402a0c36
jiko <jiko.rogy@gmail.com>
2014-11-18 22:22:02 +0900

    poyo

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char const* argv[])
 
 	GLObject<Attr_GLVersion<3,1>,Attr_GLSize<5,5,5,32,1>,ScreenSize<1920,1080>> obj;
 	obj.initialize("SDL_TITLE");
-	SDL_Delay(2000);
 	obj.close();
 	return 0;
 }
