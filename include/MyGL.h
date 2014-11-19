ab5699febaa9ef5aa3af4f7794725992d6decfd5
jiko <jiko.rogy@gmail.com>
2014-11-19 11:09:14 +0900

    fix MyGL.h

--- include/MyGL.h
+++ include/MyGL.h
@@ -9,107 +9,80 @@
 
 namespace GLLib {
 
-	template<typename Attr_GLver, typename Attr_Size, typename ScrSize>
-		class GLObject
+	//setAttribute
+	template<typename Attr_First>
+		void setAttribute()
 		{
-			private:
-				//setAttribute
-				template<typename Attr_First>
-					static void setAttribute()
-					{
-						Attr_First::SetAttr();
-					}
-				template<typename Attr_First, typename Attr_Second, typename... Rests>
-					static void setAttribute()
-					{
-						setAttribute<Attr_First>();
-						setAttribute<Attr_Second,Rests...>();
-					}
-				//member variable
-				SDL_Window* _window;
-				SDL_GLContext _context;
-				bool _is_initialized;
-				
-			public:
-				GLObject() :_is_initialized(false)
-				{
-				}
+			Attr_First::SetAttr();
+		}
+	template<typename Attr_First, typename Attr_Second, typename... Rests>
+		void setAttribute()
+		{
+			setAttribute<Attr_First>();
+			setAttribute<Attr_Second,Rests...>();
+		}
 
-				bool initialize(std::string window_title)
+
+	class GLObject
+	{
+		private:
+			//member variable
+			SDL_GLContext _context;
+			bool _is_initialized;
+
+		public:
+			GLObject() :_is_initialized(false)
+		{
+		}
+			bool initialize(SDL_Window* _window)
+			{
+				bool success = true;
+				//create context
+				_context = SDL_GL_CreateContext(_window);
+				if(_context == NULL)
 				{
-					bool success = true;
-					//exec at rumtime
-					if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+					std::cerr << "Cannot create OpenGL context: " << SDL_GetError() << std::endl;
+					success = false;
+				}
+				else
+				{
+					//initialize glew
+					glewExperimental = GL_TRUE;
+					GLenum glewError = glewInit();
+					if(glewError != GLEW_OK)
 					{
-						std::cerr << "Cannot Initialize SDL!: " << SDL_GetError() << std::endl;
+						std::cerr << "cannot initialize GLEW!: " << glewGetErrorString(glewError) << std::endl;
 						success = false;
 					}
 					else
 					{
-						//setAttribute
-						setAttribute<Attr_GLver>();
-						//create window
-						_window = SDL_CreateWindow(window_title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, ScrSize::ScrWidth, ScrSize::ScrHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_ALLOW_HIGHDPI);
-						if(_window == nullptr)
+						//use vsync
+						if(SDL_GL_SetSwapInterval(1) < 0)
 						{
-							std::cerr << "Window could not be created!: " << SDL_GetError() << std::endl;
-							success = false;
+							std::cerr << "warning: unable to set vsync!: " << SDL_GetError() << std::endl;
 						}
-						else
-						{
-							//setAttribute
-							setAttribute<Attr_Size>();
-							//create context
-							_context = SDL_GL_CreateContext(_window);
-							if(_context == NULL)
-							{
-								std::cerr << "Cannot create OpenGL context: " << SDL_GetError() << std::endl;
-								success = false;
-							}
-							else
-							{
-								//initialize glew
-								glewExperimental = GL_TRUE;
-								GLenum glewError = glewInit();
-								if(glewError != GLEW_OK)
-								{
-									std::cerr << "cannot initialize GLEW!: " << glewGetErrorString(glewError) << std::endl;
-									success = false;
-								}
-								else
-								{
-									//use vsync
-									if(SDL_GL_SetSwapInterval(1) < 0)
-									{
-										std::cerr << "warning: unable to set vsync!: " << SDL_GetError() << std::endl;
-									}
-									//OpenGL initialize complete
-									DEBUG_OUT("--- OpenGL initialize complete ---");
-									DEBUG_OUT("OpenGL Vendor: " << glGetString(GL_VENDOR));
-									DEBUG_OUT("OpenGL Renderer: " << glGetString(GL_RENDERER));
-									DEBUG_OUT("OpenGL Version: " << glGetString(GL_VERSION));
-									DEBUG_OUT(" ");
-									_is_initialized = true;
-								}
-							}
-						}
+						//OpenGL initialize complete
+						DEBUG_OUT("--- OpenGL initialize complete ---");
+						DEBUG_OUT("OpenGL Vendor: " << glGetString(GL_VENDOR));
+						DEBUG_OUT("OpenGL Renderer: " << glGetString(GL_RENDERER));
+						DEBUG_OUT("OpenGL Version: " << glGetString(GL_VERSION));
+						DEBUG_OUT(" ");
+						_is_initialized = true;
 					}
-					return success;
 				}
+				return success;
+			}
 
-				void close()
+			void close()
+			{
+				if(_is_initialized)
 				{
-					if(_is_initialized)
-					{
-						SDL_GL_DeleteContext(_context);
-						SDL_DestroyWindow(_window);
-						_window = nullptr;
-						SDL_Quit();
-						DEBUG_OUT("SDL is successfully closed.");
-						_is_initialized = false;
-					}
+					SDL_GL_DeleteContext(_context);
+					DEBUG_OUT("SDL_GL_Context is successfully closed.");
+					_is_initialized = false;
 				}
-		};
+			}
+	};
 
 } // namespace GLLib
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,24 @@
 int main(int argc, char const* argv[])
 {
 	using namespace GLLib;
+	GLObject obj;
 
-	GLObject<Attr_GLVersion<3,1>,Attr_GLSize<5,5,5,32,1>,ScreenSize<1920,1080>> obj;
-	obj.initialize("SDL_TITLE");
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cerr << "Cannot Initialize SDL!: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+	setAttribute<Attr_GLVersion<3,1>>();
+
+	SDL_Window* window = SDL_CreateWindow("SDL_Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1920, 1080, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_ALLOW_HIGHDPI);
+	if(window == NULL)
+	{
+		std::cerr << "Window could not be created!: " << SDL_GetError() << std::endl;
+	}
+	setAttribute<Attr_GLSize<5,5,32,1>>();
+	obj.initialize(window);
 	obj.close();
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
