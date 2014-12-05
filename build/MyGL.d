1cdbc636bf3fda7b27d5652b875aed42fb5249e5
jiko <jiko.rogy@gmail.com>
2014-12-05 11:38:27 +0900

    fixed main func arg

--- build/MyGL.d
+++
@@ -1,8 +0,0 @@
-build/MyGL.o: src/MyGL.cpp src/../include/MyGL.h \
-  src/../include/HelperClass.h src/../include/gl_debug.h
-
-src/../include/MyGL.h:
-
-src/../include/HelperClass.h:
-
-src/../include/gl_debug.h:
