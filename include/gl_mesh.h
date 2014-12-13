93c2c117ff4819074a4cae6932dc84a679303e73
jiko <jiko.rogy@gmail.com>
2014-12-13 19:40:23 +0900

    fixed some

--- include/gl_mesh.h
+++ include/gl_mesh.h
@@ -2,6 +2,8 @@
 
 #include "gl_helper.h"
 #include "gl_base.h"
+#include "gl_debug.h"
+#include <vector>
 
 namespace jikoLib{
 	namespace GLLib{
@@ -9,7 +11,8 @@ namespace jikoLib{
 		class Mesh
 		{
 			private:
-
+				VertexBuffer<ArrayBuffer, StaticDraw, GLAllocator<Alloc_VertexBuffer>> vertex;
+				VertexBuffer<ArrayBuffer, StaticDraw, GLAllocator<Alloc_VertexBuffer>> normal;
 			public:
 		};
 
--- src/gl_debug.cpp
+++ src/gl_debug.cpp
@@ -1,5 +1,6 @@
 #include "../include/gl_base.h"
 #include "../include/gl_debug.h"
+#include "../include/gl_mesh.h"
 
 #ifdef DEBUG
 GLenum jikoLib::GLLib::_err_;
