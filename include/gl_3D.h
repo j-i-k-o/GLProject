2d5e5eeb9239e7d7ae0fca48357a7e1275ca74b6
jiko <jiko.rogy@gmail.com>
2014-12-24 04:59:14 +0900

    poyo

--- include/gl_3D.h
+++ include/gl_3D.h
@@ -9,8 +9,42 @@ namespace jikoLib{
 		
 		class Mesh3D{
 			private:
-				
+				VBO vertex;
+				VBO normal;
+				VBO texcrd;
+				IBO index;
+				VAO v_array;
 			public:
+				inline const VBO& getVertex() const
+				{
+					return vertex;
+				}
+				inline const VBO& getNormal() const
+				{
+					return normal;
+				}
+				inline const VBO& getTexcrd() const
+				{
+					return texcrd;
+				}
+				inline const VAO& getVArray() const
+				{
+					return v_array;
+				}
+
+				inline bool getIsSetArray() const
+				{
+					return 
+						vertex.getisSetArray() &&
+						normal.getisSetArray() &&
+						texcrd.getisSetArray();
+				}
+
+				Mesh3D operator+(const Mesh3D &obj)
+				{
+
+				}
+
 		};
 	}
 }
