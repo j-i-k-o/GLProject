6c10ed4476f99b83cb329bd2fe361eca6399c0bd
jiko <jiko.rogy@gmail.com>
2014-12-25 11:35:16 +0900

    poyo

--- include/gl_3D.h
+++ include/gl_3D.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <cmath>
 #include "gl_helper.h"
 #include "gl_base.h"
 #include "gl_debug.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing fla
 
 namespace jikoLib{
 	namespace GLLib{
@@ -14,7 +24,18 @@ namespace jikoLib{
 				VBO texcrd;
 				IBO index;
 				VAO v_array;
+
+				glm::vec3 pos;
+				glm::quat rot;
+				glm::vec3 scale;
+
 			public:
+
+				Mesh3D()
+					:pos(0.0f, 0.0f, 0.0f),
+					 rot(1.0f, 0.0f, 0.0f, 0.0f),
+					 scale(1.0f, 1.0f, 1.0f)  {}
+
 				inline const VBO& getVertex() const
 				{
 					return vertex;
@@ -83,7 +104,125 @@ namespace jikoLib{
 						index.copyData(ind, Size_Elem);
 					}
 
+				inline void setPos(const glm::vec3 &vec)
+				{
+					this->pos = vec;
+				}
 
+				inline const glm::vec3& getPos()
+				{
+					return this->pos;
+				}
+
+				inline void setScale(const glm::vec3 &vec)
+				{
+					this->scale = vec;
+				}
+
+				inline const glm::vec3& getScale()
+				{
+					return this->scale;
+				}
+
+				inline void rotate(const glm::vec3 &axis, GLfloat angle)
+				{
+					this->rot = glm::rotate(this->rot, angle, glm::normalize(axis));
+				}
+
+				inline void resetRot()
+				{
+					this->rot = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+				}
+
+				inline const glm::quat& getRot()
+				{
+					return this->rot;
+				}
+
+				glm::mat4 getModelMatrix()
+				{
+					glm::mat4 mat = glm::mat4_cast(rot);
+					glm::mat4 poyo = mat*glm::scale(glm::mat4(), scale);
+					return glm::translate(poyo, glm::inverse(glm::mat3(poyo))*pos);
+				}
+		};
+
+		class Camera{
+			private:
+				glm::vec3 pos; //position
+				glm::vec3 drct;  //direction
+				glm::vec3 up; //up
+
+				GLfloat fovy;
+				GLfloat aspect;
+				GLfloat _near;
+				GLfloat _far;
+
+				glm::mat4 viewMat;
+				glm::mat4 projMat;
+			public:
+
+				Camera()
+					:pos(0.0f, 0.0f, 0.0f),
+					drct(1.0f, 0.0f, 0.0f),
+					up(0.0f, 1.0f, 0.0f),
+					fovy(M_PI_4),
+					aspect(1.0f),
+					_near(0.1f),
+					_far(10.0f) {}
+
+				inline void setPos(const glm::vec3 &pos)
+				{
+					this->pos = pos;
+				}
+				inline void setDrct(const glm::vec3 &drct)
+				{
+					this->drct = drct;
+				}
+				inline void setUp(const glm::vec3 &up)
+				{
+					this->up = up;
+				}
+				inline void setFovy(GLfloat fovy)
+				{
+					this->fovy = fovy;
+				}
+				inline void setAspect(GLfloat aspect)
+				{
+					this->aspect = aspect;
+				}
+				inline void setAspect(GLfloat width, GLfloat height)
+				{
+					this->aspect = width / height;
+				}
+				inline void setNear(GLfloat _near)
+				{
+					this->_near = _near;
+				}
+				inline void setFar(GLfloat _far)
+				{
+					this->_far = _far;
+				}
+				inline const glm::vec3& getPos() const
+				{
+					return this->pos;
+				}
+				inline const glm::vec3& getDrct() const
+				{
+					return this->drct;
+				}
+
+				const glm::mat4& getViewMatrix()
+				{
+					viewMat = glm::lookAt(pos, drct, up);
+					return viewMat;
+				}
+
+				const glm::mat4& getProjectionMatrix()
+				{
+					projMat = glm::perspective(fovy, aspect, _near, _far);
+					return projMat;
+				}
 		};
 	}
 }
