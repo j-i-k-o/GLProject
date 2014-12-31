9de36607974f98a3f938061bda2c78d89c3cf492
jiko <jiko.rogy@gmail.com>
2014-12-31 15:06:01 +0900

    add texture parameter

--- sample/include/gl_all.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "gl_base.h"
-#include "gl_3D.h"
-#include "gl_main.h"
--- sample/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/gl_all.h"
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-
-
